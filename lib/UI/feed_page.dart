c78fc546eada2bc63e8274f29331cb12e130ae14
llherrera <95147465+llherrera@users.noreply.github.com>
2023-04-04 11:39:56 -0500

    feedPage

--- lib/UI/feed_page.dart
+++ lib/UI/feed_page.dart
@@ -3,10 +3,77 @@ import 'package:flutter/material.dart';
 class FeedPage extends StatelessWidget {
   const FeedPage({super.key});
 
+  Future<List<String>> _searchImages(String term) async {
+    await Future.delayed(Duration(seconds: 1));
+    return List.generate(20, (index) => 'https://source.unsplash.com/random/800x800/?$term');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('feed'),
+    return Stack(
+      children: <Widget>[
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  color: Colors.blueGrey,
+                  child: Row(
+                    children: <Widget>[
+                      const Spacer(flex: 1),
+                      const Expanded(
+                        flex: 8,
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey,
+                            hintText: 'Search',
+                            border: InputBorder.none,
+                          ),
+                        )
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.search),
+                          onPressed: () {},
+                      ),
+                    ],
+                  )
+                ),
+                photos(),
+              ],
+            ),
+          )
+        )
+      ],
+    );
+  }
+
+  Widget photos(){
+    return FutureBuilder(
+      future: _searchImages('food'),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          return GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Image.network(
+                snapshot.data![index],
+                fit: BoxFit.cover,
+              );
+            },
+          );
+        } else {
+          return const Center(child: Text('No se encontraron resultados.'));
+        }
+      },
     );
   }
 }
