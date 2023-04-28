0ea8ce0f50adab5844dfef54b1ec102b131ef07d
Nicolas <nicolasbernal@uninorte.edu.co>
2023-04-27 20:50:17 -0500

    mission proximity and notification

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import '../widgets/filter_widget.dart';
 import './feed_page.dart';
 import './profile_page.dart';
@@ -67,7 +69,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
+  bool quest = true;
+  final picker = ImagePicker();
   String _search = '';
 
   void setSearch(String search) {
@@ -76,7 +79,15 @@ class _HomeState extends State<Home> {
     });
   }
 
-  Position _currentPosition = Position(longitude: -74.78132, latitude: 10.96854, timestamp: DateTime.now(), accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0);
+  Position _currentPosition = Position(
+      longitude: -74.78132,
+      latitude: 10.96854,
+      timestamp: DateTime.now(),
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
   late GoogleMapController _mapController;
 
   void _getCurrentLocation() async {
@@ -125,10 +136,30 @@ class _HomeState extends State<Home> {
     );
   }
 
+  void checkProximityMission() async {
+    double destLat1 = 37.7749;
+    double destLat2 = destLat1;
+    double destLng1 = -122.4194;
+    double destLng2 = destLng1;
+
+    // Calcular la distancia entre la ubicación actual del usuario y la ubicación de destino utilizando la fórmula Haversine
+    double distance = await Geolocator.distanceBetween(
+        destLat1, destLng1, destLat2, destLng2);
+
+    // Comprobar si la distancia es menor que un umbral predefinido
+    double proximityThreshold = 1000.0; // metros
+    if (distance < proximityThreshold) {
+      quest = true;
+    }
+  }
+
+  void camara() async {
+    await picker.getImage(source: ImageSource.camera);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return 
-    Stack(
+    return Stack(
       children: <Widget>[
         Column(
           children: <Widget>[
@@ -136,39 +167,89 @@ class _HomeState extends State<Home> {
               flex: 2,
               child: _buildMap(),
             ),
-            const Flexible(
-              flex: 1,
-              child: Filter()
-            )
+            const Flexible(flex: 1, child: Filter())
           ],
         ),
         Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: Row(
-            children: <Widget>[
-              IconButton(onPressed: (){}, icon: const Icon(Icons.settings_outlined, size: 40,)),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20),
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey,
-                    hintText: 'Search',
-                    hintStyle: TextStyle(fontSize: 20),
-                    border: InputBorder.none,
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.settings_outlined,
+                          size: 40,
+                        )),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(fontSize: 20),
+                          decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey,
+                            hintText: 'Search',
+                            hintStyle: TextStyle(fontSize: 20),
+                            border: InputBorder.none,
+                          ),
+                          onChanged: (value) => setSearch(value),
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          checkProximityMission();
+                        },
+                        icon: const Icon(
+                          Icons.search,
+                          size: 40,
+                        )),
+                  ],
+                ))),
+        Visibility(
+          visible: quest,
+          child: Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              color: Color(0xFFF07B2B),
+              child: Column(
+                children: [
+                  Text(
+                    '¡Has llegado a tu destino! Toma una foto y reclama tu recompensa.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                  onChanged: (value) => setSearch(value),
-                )
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      camara();
+                    },
+                    icon: Icon(Icons.camera_alt),
+                    label: Text(
+                      'Cámara',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFFF7A935),
+                    ),
+                  ),
+                ],
               ),
-              IconButton(onPressed: (){}, icon: const Icon(Icons.search, size: 40,)),
-            ],
-          ))
+            ),
+          ),
         ),
       ],
     );
